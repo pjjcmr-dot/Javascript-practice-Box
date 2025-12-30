8ce22c57a578ed0717c0273eb1c3e44c588df0b0
박종장 <pjjcmr@gmail.com>
2025-12-30 15:13:48 +0900

    10th

---
+++ practiceModern.js
@@ -0,0 +1,47 @@
+// function sayHi() {
+//   console.log("Hi!");
+// }
+
+// sayHi();
+
+// (function () {
+//   console.log("Hi!");
+// })();
+
+// (function (x, y) {
+//   console.log(x + y);
+// })(3, 5);
+
+// (function sayHi(y) {
+//   console.log('Hi!'+y);
+// })('5');
+
+// // ReferenceError
+
+// (function init() {
+//   // 프로그램이 실행 될 때 기본적으로 동작할 코드들..
+// })();
+
+// const firstName = 'Young';
+// const lastName = 'Kang';
+
+// const greetingMessage = function () {
+//   const fullName = `${firstName} ${lastName} `;
+
+//   return `Hi! My name is ${fullName}`;
+// };
+// console.log(greetingMessage());
+
+// function firstWords() {
+//   let word = "";
+//   for (const arg of arguments) {
+//     word += arg[0];
+//   }
+//   console.log(word);
+// }
+
+// firstWords("나만", "없어", "고양이");
+// firstWords("아니", "바나나말고", "라면먹어");
+// firstWords("만두", "반으로", "잘라먹네", "부지런하다");
+// firstWords("결국", "자바스크립트가", "해피한", "지름길");
+// firstWords("빨간색", "주황색", "노란색", "초록색", "파란색", "남색", "보라색");
