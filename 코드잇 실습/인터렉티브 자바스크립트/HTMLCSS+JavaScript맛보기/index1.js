527ecfa96477cbeae559c33c0054aa6bd3848d98
박종장 <pjjcmr@gmail.com>
2026-01-01 21:51:15 +0900

    13-4th

---
+++ 코드잇 실습/인터렉티브 자바스크립트/HTMLCSS+JavaScript맛보기/index1.js
@@ -0,0 +1,16 @@
+//* id로 태그 선택하기
+const myTag1 = document.getElementById("myNumber");
+console.log(myTag1);
+//
+//
+//
+//
+//
+//
+//
+const myTag2 = document.getElementById("btns");
+console.log(myTag2);
+//
+//
+const myTag3 = document.getElementById("codeit");
+console.log(myTag3);
