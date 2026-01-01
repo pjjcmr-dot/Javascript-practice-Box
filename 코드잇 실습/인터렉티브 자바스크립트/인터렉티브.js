59d3aab6294d05c3fc278b8c919322dae4302eb1
박종장 <pjjcmr@gmail.com>
2026-01-01 19:10:20 +0900

    13-3th

---
+++ 코드잇 실습/인터렉티브 자바스크립트/인터렉티브.js
@@ -0,0 +1,28 @@
+
+//* [window]
+//*    │
+//*    └── [document.XXXXXXXXXX]  ← 페이지 전체 관리자
+//*           │
+//*           ├── 요소(#title)
+//*           └── 요소(.btn)
+
+
+
+//TODO: | 요소 1개 찾기 | `document.querySelector("선택자")` |  |
+//TODO: | id로 찾기 | `document.getElementById("id")` |  |
+//TODO: | 요소 만들기 | `document.createElement("태그이름")` |  |
+//TODO: | 클릭 감지 | `addEventListener("click", ...)` |  |
+
+
+
+
+
+
+
+
+
+
+
+
+
+
