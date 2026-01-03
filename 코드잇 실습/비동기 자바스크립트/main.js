5cbb2a434165fe8058758df4d3889b457f84de88
박종장 <pjjcmr@gmail.com>
2026-01-03 18:49:24 +0900

    14-3th

---
+++ 코드잇 실습/비동기 자바스크립트/main.js
@@ -0,0 +1,12 @@
+function sayHello() {
+  console.log('Hello World!');
+}
+
+function sayGoodbye() {
+  console.log('Goodbye World!');
+}
+
+function printMessage(func) {
+  console.log('Printing message...');
+  func();
+}
